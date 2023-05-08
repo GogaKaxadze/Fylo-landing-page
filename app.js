b4a05e23f79d0cb94454eca5f179117b874096f8
GogaKaxadze <101425184+GogaKaxadze@users.noreply.github.com>
2023-05-08 05:20:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const getStarted = document.querySelector(".header-button");
+const lastBtn = document.querySelector(".main-button")
 const enterEmailInput = document.querySelector("#header-email");
 const mailCheck = document.querySelector("#check-mail");
 const mailEnter = document.querySelector("#enter-email");
+
+const secondMailCheck = document.getElementById("second-check");
+const secondEnter = document.getElementById("second-enter")
+
+const lastEmail = document.getElementById("main-email")
+const secondForm = document.querySelector(".form2")
 const form = document.querySelector("#form");
+
+
+secondForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+})
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -25,3 +37,22 @@ getStarted.addEventListener("click", () => {
     showCheck();
   }
 });
+
+
+function secondShowError() {
+  secondEnter.classList.remove("hidden");
+  lastEmail.style.borderColor = "red";
+  secondMailCheck.classList.add("hidden");
+}
+function secondShowCheck() {
+  secondMailCheck.classList.remove("hidden");
+  lastEmail.style.borderColor = "green";
+  secondEnter.classList.add("hidden");
+}
+lastBtn.addEventListener("click", () => {
+  if(lastEmail.value == ""){
+    secondShowError();
+  }else{
+    secondShowCheck();
+  }
+})
